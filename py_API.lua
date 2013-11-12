639293ddb153e01a4a8e09c93a7da4be9f7fdcc0
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2013-11-12 12:01:15 +0900

    Create py_API.lua

---
+++ py_API.lua
@@ -0,0 +1,25 @@
+function repr(obj)
+
+end
+
+function print(...)
+    local sep = " "
+    for var in arg do
+        write(tostring(var))
+        write(sep)
+    end
+
+    write("\n")
+end
+
+function string.split()
+
+end
+
+function string.startswith()
+
+end
+
+function table.items()
+
+end
